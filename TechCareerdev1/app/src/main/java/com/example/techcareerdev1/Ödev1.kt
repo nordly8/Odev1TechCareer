110e7c94177189ad22f2ff35103f1b58253fda1e
nordly8 <109112876+nordly8@users.noreply.github.com>
2023-03-21 11:45:13 +0300

    TechCareerÖdev1
    Ödev1

---
+++ TechCareerdev1/app/src/main/java/com/example/techcareerdev1/Ödev1.kt
@@ -0,0 +1,54 @@
+package com.example.techcareerdev1
+
+fun main() {
+
+    var sehir = "GAZİANTEP"
+    var ulke = "TÜRKİYE"
+    var telefon = 5465434505
+    var posta_kodu = 27027
+    var email = "kasimacikgoz4@gmail.com"
+    var meslek = "ELEKTRİK ELEKRTONİK MÜHENDİSİ"
+    var stok_miktrari = 100.000
+    var musteri_adi = "Kasım Açıkgöz"
+    var bakiye = 100
+    var dogum_gunu = "20 ocak"
+    var maas = 15000
+    var medeni_durum = "Bekar"
+    var urun_yorum = "Kaliteli"
+    var odeme_tarihi = "15.06.2023"
+    var odeme = 100.000
+    var siparis_adedi = 250.000
+    var araba_modeli = "Opel Astra"
+    var kitap_adi = "İki Şehrin Hikayesi"
+    var yayinlama_tarihi = "25.06.2023"
+    var indirim_miktari = 25.000
+    var oda_sayisi = 3
+    var enlem = 37.028372
+    var boylam = 37.320548
+    var urun_adi = "Televizyon"
+    var yemek_fiyati = "50₺"
+    var marka = "Microsoft"
+    var muzik_adi = "Metallica - One"
+    var video_suresi = "7.15 dk"
+    var urun_puani = 100
+    var resim_adi = "Van Gogh - Starry Night"
+    var dosya_formati = ".JPG"
+    var renk = "Mavi"
+    var renk_kodu = "#0000ff"
+    var telefon_modeli = "Oppo Reno 5 Lite"
+    var ekran_boyutu = 5.17
+    var agirlik = "80 kg"
+    var ulusal_gun = "29 Ekim"
+    var tail_gunu = "23 Nisan"
+    var rezervasyon_tarihi = "15.08.2023"
+    var sokak_adi = "Güneykent"
+    var otobus_hatti = "Güneykent-A104"
+    var kalan_dakika = "55 dk"
+    var takip_kodu = 2548958852
+    var kupon_suresi = "1 ay"
+    var kupon_kodu = 569874585478
+    var fatura_adresi = "Güneykent mah. Emko Sit. Daire:5 Şahinbey/Gaziantep"
+
+
+
+}
